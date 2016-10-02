fcf81c617178929b0ff2994a636e8ca4c4a9c03a
gabe <gj+code@mail.co.de>
2016-10-02 03:25:02 -0400

    Done.

--- spec/layouts/admin_layout_spec.rb
+++ spec/layouts/admin_layout_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe do
   it "should include the main file in the admin layout" do
     expect(File.read("app/views/layouts/admin.html.erb")).to include("stylesheet_link_tag 'posts'")
   end
+
+  it "should include the remaining CSS file (posts.css) in the admin layout" do
+    expect(File.read("app/views/layouts/admin.html.erb")).to include("stylesheet_link_tag 'admin'")
+  end
 end
