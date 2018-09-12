0d1311b46a6e5aab10480cfb2378777e4827b3ca
Michele Conway <mconway890@gmail.com>
2018-09-12 14:34:06 -0400

    Done.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
+Rails.application.config.assets.precompile += %w( admin.css )
+Rails.application.config.assets.precompile += %w( posts.css )
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
